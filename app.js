3f00ab8c50f2c69bfcd8798141600ebb222260da
gustavocsak <78715220+gustavocsak@users.noreply.github.com>
2022-03-13 00:07:30 -0800

    Fixed scroll to wrong element/#12

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const menuButton = document.getElementsByClassName('toggle-button')[0];
 const links = document.getElementsByTagName('nav')[0];
 
+const intViewportWidth = window.innerWidth;
+
 menuButton.addEventListener('click', () => {
     links.classList.toggle('active');
 });
 
 /* Smooth scrolling to section when clicking navigation anchor tags */
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+document.querySelectorAll('.link').forEach((anchor) => {
     anchor.addEventListener('click', function (e) {
+        links.classList.toggle('active');
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
