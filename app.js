9b89cb01b2729cfe370e03a29d42d62a02c7fd3f
gustavocsak <78715220+gustavocsak@users.noreply.github.com>
2022-03-12 23:45:58 -0800

    Added smooth scroll to nav links/#12

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const links = document.getElementsByTagName('nav')[0];
 menuButton.addEventListener('click', () => {
     links.classList.toggle('active');
 });
+
+/* Smooth scrolling to section when clicking navigation anchor tags */
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+        });
+    });
+});
